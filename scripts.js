ba6116d67c25c6cc73f1ea6839ea2a1894710230
Jami-tsm <jameson.oliver.baker@gmail.com>
2025-06-02 13:35:02 -0400

    Switched to multi-page design. Added "to-top" button to all pages, but it is not showing up, needs further investigation. Added contact form embed from 123formbuilder, getting 403 error, will watch yt vid.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,20 @@ function toggleMenu(){
     icon.classList.toggle("open")
 
 }
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
