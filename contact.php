176f8c42c015a3e9a18acfacf2d577fd2a1181a3
Jami-tsm <jjb1390@g.rit.edu>
2025-10-08 22:55:32 -0400

    Updated positions and alumni status on brotherhood. Working on contact form

--- contact.php
+++ contact.php
@@ -14,8 +14,7 @@ $to = "jjb1390@g.rit.edu";
 $txt = "Name = " . $name . "\r\n  Email = "
     . $email . "\r\n Message =" . $message;
 
-$headers = "From: noreply@demosite.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
+$headers = "From: contact@pdtnewyorketa.com" . "\r\n";
 if ($email != NULL) {
     mail($to, $subject, $txt, $headers);
 }
